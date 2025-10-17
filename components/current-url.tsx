1ca71672fccaad339768e0adeecec2186ca6e847
Roberto Amarante <robertamarantec@gmail.com>
2025-10-17 17:57:32 -0400

    Fix current URL

--- components/current-url.tsx
+++ components/current-url.tsx
@@ -1,11 +1,16 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import { usePathname } from "next/navigation"
 
 export function CurrentUrl() {
   const pathname = usePathname()
-  const host = process.env.NEXT_PUBLIC_SITE_URL || window.location.origin
-  const url = host + pathname
+  const [url, setUrl] = useState("")
+
+  useEffect(() => {
+    const host = process.env.NEXT_PUBLIC_SITE_URL || window.location.origin
+    setUrl(host + pathname)
+  }, [pathname])
 
   return <p>{url}</p>
 }
